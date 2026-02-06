86de1062bbecb4d4714b4d2f3e63a8dd65399ba7
Ivanilson Ferreira de Oliveira <ivanilson.ferreira.mec@gmail.com>
2026-02-06 11:22:04 -0300

    feat(setup): create

---
+++ HtmlAnalyzer.java
@@ -0,0 +1,10 @@
+public class HtmlAnalyzer {
+  public static void main(String[] args) throws Exception {
+    if (args.length != 1) {
+      System.out.println("Invalid");
+      return;
+    }
+    // TODO: implementar a lógica real do teste aqui.
+    System.out.println("Invalid");
+  }
+}
